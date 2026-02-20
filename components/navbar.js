d21ac871da26e2f7aaae0355d03c216376cd078c
Luke-Yamasaki <lukeyamasaki@gmail.com>
2026-02-20 19:23:41 +0900

    fix: resolve nested a tag inside link

--- components/navbar.js
+++ components/navbar.js
@@ -3,46 +3,41 @@ import Link from "next/link";
 import Image from "next/image";
 
 //Components
-import { NavLinks } from './navLinks';
-import { HamburgerMenu } from './hamburgerMenu';
+import { NavLinks } from "./navLinks";
+import { HamburgerMenu } from "./hamburgerMenu";
 
 //CSS
-import styles from '../styles/navigation.module.css';
+import styles from "../styles/navigation.module.css";
 
 //Images
-import logo from '../public/images/LKY_Logo.png';
+import logo from "../public/images/LKY_Logo.png";
 
 export const NavBar = ({ visited }) => {
-
-    return (
-        <nav className={visited ? styles.regNav : styles.welcomeNav}>
-            <ul className={styles.navList}>
-                <li className={`${styles.navItem} ${styles.navLogo}`}>
-                    <Link href='/'>
-                        <a className={styles.homeLink}>
-                            <div className={styles.logoBox}>
-                                <Image
-                                    priority
-                                    className={styles.logo}
-                                    src={logo}
-                                    height={30}
-                                    width={30}
-                                    alt="Luke Yamasaki's logo"
-                                />
-                                <div className={styles.logoType}>
-                                    LUKE YAMASAKI
-                                </div>
-                            </div>
-                        </a>
-                    </Link>
-                </li>
-                <li className={`${styles.navItem} ${styles.navMenu}`}>
-                    <HamburgerMenu />
-                </li>
-                <li className={`${styles.navItem} ${styles.navLinks}`}>
-                    <NavLinks />
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className={visited ? styles.regNav : styles.welcomeNav}>
+      <ul className={styles.navList}>
+        <li className={`${styles.navItem} ${styles.navLogo}`}>
+          <Link href="/" className={styles.homeLink}>
+            <div className={styles.logoBox}>
+              <Image
+                priority
+                className={styles.logo}
+                src={logo}
+                height={30}
+                width={30}
+                alt="Luke Yamasaki's logo"
+              />
+              <div className={styles.logoType}>LUKE YAMASAKI</div>
+            </div>
+          </Link>
+        </li>
+        <li className={`${styles.navItem} ${styles.navMenu}`}>
+          <HamburgerMenu />
+        </li>
+        <li className={`${styles.navItem} ${styles.navLinks}`}>
+          <NavLinks />
+        </li>
+      </ul>
+    </nav>
+  );
+};
